12873cba5e86be31405d68b46d131eceea2858be
Rainer Money <emilmammadov06@gmail.com>
2024-07-01 13:29:22 +0400

    Getting there

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import CryptoTransactionParser
-import  CryptoTransactionResults
+import CryptoTransactionResults
 import ConvertCrypto
 import ParseTheFinalResults
 
@@ -11,44 +11,35 @@ def process_results(results_sent, results_received):
     # Process results_sent
     for key, value in results_sent.items():
         if value != 0:
-            dollar_sent[key] = value
+            dollarAmt = float(value) * ConvertCrypto.ConvertCrypto().GetPrice(key)
+            dollar_sent[key] = dollarAmt
 
     # Print or use the dollar_sent dictionary as needed
     print("Dollar Sent:")
     for key, value in dollar_sent.items():
-        print(f"{key}, :{value}")
-        #print(f"{key} {ConvertCrypto.ConvertCrypto().GetPrice({key})} dollars")
+        print(f"{key}: ${value:.2f}")
 
     # Process results_received
     for key, value in results_received.items():
         if value != 0:
-            dollar_received[key] = value
+            dollarAmt = float(value) * ConvertCrypto.ConvertCrypto().GetPrice(key)
+            dollar_received[key] = dollarAmt
 
-    ##print("Dollar Received:")
+    print("\nDollar Received:")
     for key, value in dollar_received.items():
-        print(f"{key}, : {value}")
-        #print(f"{key} {ConvertCrypto.ConvertCrypto().GetPrice({key})} dollars")
+        print(f"{key}: ${value:.2f}")
 
 
 if __name__ == "__main__":
     parser = CryptoTransactionParser.CryptoTransactionParser()
     parser.parse_transactions("https://t.me/s/whale_alert_io")
+
     searcher = CryptoTransactionResults.CryptoCoinTransactionResults()
-    # Read data from results.txt
     with open('results.txt', 'r') as file:
         data = file.read()
     searcher.parse_data(data)
     searcher.write_results('final_results.txt')
-    #btcPrice = ConvertCrypto.ConvertCrypto().GetPrice("Bitcoin")
-    #print(f"BTC {btcPrice}")
 
     parser = ParseTheFinalResults.ParseTheFinalResults()
     results_sent, results_received = parser.FinalResultsParser()
     process_results(results_sent, results_received)
-
-
-
-
-
-
-
